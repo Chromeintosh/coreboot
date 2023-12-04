58f4c3f4a07aaf96941543302b7fd9ff9b29cf6a
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2023-12-04 11:49:22 -0600

    Do the thing with the stuff

--- build-uefi.sh
+++ build-uefi.sh
@@ -8,7 +8,7 @@ platforms=('snb_ivb' 'hsw' 'byt' 'bdw' 'bsw' 'skl' 'apl' 'kbl' 'whl' 'glk' \
 build_targets=()
 
 json_file=cbmodels.json
-rom_path=https://www.mrchromebox.tech/files/firmware/full_rom/
+rom_path=https://ethanthesleepy.one/public/mac_build/
 echo -e "{" > $json_file
 
 if [ -z "$1" ]; then
@@ -21,25 +21,31 @@ else
 	build_targets=($@)
 fi
 
+mkdir -p macos/
+failed_builds=macos/failed.log
+touch $failed_builds
+
 for device in "${build_targets[@]}"; do
 	filename="coreboot_edk2-${device}-mrchromebox_$(date +"%Y%m%d")_macos.rom"
-	rm -f ~/dev/firmware/${filename}*
+        rm -f macos/${filename}*
 	rm -rf ./build
 	cfg_file=$(find ./configs -name "config.$device.uefi")
 	cp "$cfg_file" .config
 	echo "CONFIG_LOCALVERSION=\"MrChromebox-4.21.0-macOS-v1\"" >> .config
-	echo "# DISABLE_HECI1_AT_PRE_BOOT is not set" >> .config
 	make clean
 	make olddefconfig
-	make -j$(nproc)
+        if  ! make -j$(nproc); then
+                echo -e "${device}" >> $failed_builds
+                continue
+        fi
 	cp ./build/coreboot.rom ./${filename}
 	sha1sum ${filename} > ${filename}.sha1
 	echo -e "\t\"${device}\": {" >> $json_file
 	echo -e "\t\t\"url\": \"${rom_path}${filename}\"," >> $json_file
 	echo -e "\t\t\"sha1\": \"$(cat ${filename}.sha1 | awk 'NR==1{print $1}')\"" >> $json_file
 	echo -e "\t}," >> $json_file
-	mv ${filename}* ~/dev/firmware/
+        mv ${filename}* macos/
 done
 echo -e "}" >> $json_file
-# remove last comma
-sed -i 's/\(.*\),/\1/' $json_file
+
+cp "${json_file}" macos/${json_file}
